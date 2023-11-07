9b1f0d990a673318d8e95b3304a3fe429854e697
Kocia <annazofka@gmail.com>
2023-11-07 04:24:03 +0000

    add comments to 'delete occureneces of an element' kata

--- codewars-javascript/6kyu/delete-occurences-of-an-element.js
+++ codewars-javascript/6kyu/delete-occurences-of-an-element.js
@@ -12,26 +12,44 @@ With list [20,37,20,21] and number 1, the result would be [20,37,21].
 
 // My solution:
 function deleteNth(arr, n) {
+	// create an empty array to store the result of the function
 	const result = [];
+	// iterate through each element of arr and then execute the code inside the loop for each elem one by one
 	for (let elem of arr) {
+		// declare a new variable called 'occurence' to store the result of filter method in a new array and check its length.
+		// Return true if current elem of arr in the loop is equal to any of the 'item'(s) of 'result' array. Then calculate the length of the filtered array. This effectively counts how many times elem appears in the 'result' array.
 		const occurence = result.filter(item => item === elem).length;
+		// check if occurrence exists and if it's equal to n
 		if (occurence && occurence === n) {
+			// if occurrence equals n skip the current iteration of the loop and move to the next element in arr
 			continue;
 		} else {
+			// if statement is not met, push the current element 'elem' into the result array
 			result.push(elem);
 		}
 	}
 	return result;
 }
 
+deleteNth([20, 21, 36, 21, 22, 36], 1);
+
+// 1st iteration
+// elem will be 20, but the 'result' array is empty so filter will result in undefined which is falsy. The occurence array will therefore be [].length so 0. In the if statement, 0 will evaluate to false and occurence === n will also evaluate to false, so the elem will be pushed to the result array.
+
+// 4th iteration
+// elem will be 21, but after filter the length of 'occurence' array will be 1 i.e. [21].length
+
 // Model solution:
 function deleteNth(arr, x) {
+	// Create an empty object to store how many times each object exists
 	var cache = {};
 	return arr.filter(function (n) {
 		cache[n] = (cache[n] || 0) + 1;
 		return cache[n] <= x;
 	});
 }
+
+// In the first iteration, n will be 1, and cache will be equal to {}, so cache[n] will be undefined. It is setting it to (cache[num] || 0) which means if cache[num] OR 0. As cache[num] is undefined in the first instance, it'll be 0. It is then adding 1. Each time the number is hit, it is adding one, and then it will return if the number of instances is below or equal the accepted number.When it goes above that threshold, it'll be equal to false and now it won't be included as part of the filter.
 
 // Model solution 2:
 const deleteNth = (a, x) => {
@@ -45,5 +63,20 @@ function deleteNth(arr, x) {
 	return arr.filter(function (number) {
 		obj[number] = obj[number] ? obj[number] + 1 : 1;
 		return obj[number] <= x;
+	});
+}
+
+// My favourite solution:
+function deleteNth(arr, n) {
+	const sorted = {};
+
+	return arr.filter(e => {
+		if (sorted[e]) {
+			sorted[e].push(e);
+			return sorted[e].length > n ? null : e;
+		} else {
+			sorted[e] = [e];
+			return e;
+		}
 	});
 }
