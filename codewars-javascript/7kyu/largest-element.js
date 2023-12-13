695486204dc2f61e909f9e70693b85f85b39eb90
Ania <annazofka@gmail.com>
2023-12-13 08:28:55 -0800

    add largest element Kata solution

---
+++ codewars-javascript/7kyu/largest-element.js
@@ -0,0 +1,20 @@
+// Write a program that outputs the top n elements from a list.
+
+// Example:
+
+// largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+
+// My solution:
+function largest(n, array) {
+  return n > 0 ? array.sort((a, b) => a - b).slice(-n) : [];
+}
+
+// Other solutions:
+function largest(n, xs) {
+  return xs
+    .sort(function (a, b) {
+      return a - b;
+    })
+    .slice(xs.length - n);
+}
