235cfe88f5c42e24b0c94b94082e90ec64a23f19
Ania <annazofka@gmail.com>
2024-01-22 10:01:59 -0800

    add Correct the mistakes of the character recognition software Kata solution

---
+++ codewars-javascript/8kyu/correct-mistakes-of-the-character-recognition-software.js
@@ -0,0 +1,37 @@
+/* Instructions:
+Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake. */
+
+// My solution:
+function correct(string) {
+  return string.replace(/1/g, "I").replace(/0/g, "O").replace(/5/g, "S");
+}
+
+// Solution 2:
+const corrections = {
+  5: "S",
+  0: "O",
+  1: "I",
+};
+
+const correct = (string) =>
+  string.replace(/[501]/g, (character) => corrections[character]);
+
+// Solution 3:
+function correct(s) {
+  s = s.split("");
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "5") s[i] = "S";
+    else if (s[i] === "0") s[i] = "O";
+    else if (s[i] === "1") s[i] = "I";
+  }
+  return s.join("");
+}
